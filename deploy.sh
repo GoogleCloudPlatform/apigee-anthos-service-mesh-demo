4b99aa25d59a19f06da99a54b49b54e7e3077664
tyayers <tyler.ayers@gmail.com>
2022-04-04 17:23:26 +0200

    fixed gcloud project issue

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,7 @@ kubectl apply -f kubernetes-manifests/proxy-service.yaml
 # Build and deploy portal
 PROJECT=$(gcloud config get-value project)
 gcloud builds submit --tag eu.gcr.io/$PROJECT/apigee-service-portal
+gcloud run deploy apigee-service-portal --image eu.gcr.io/$PROJECT/apigee-service-portal --platform managed --project $PROJECT \
+  --region europe-west1 --allow-unauthenticated
 
 # TODO deploy Apigee proxy
