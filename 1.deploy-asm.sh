05c29ad37cf4e1685eff29d27317020b81007e68
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2022-04-11 12:20:11 -0700

    set -e

--- 1.deploy-asm.sh
+++ 1.deploy-asm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "$PROJECT" ]
 then
 echo "No PROJECT variable set"
--- 3.deploy-apigee.sh
+++ 3.deploy-apigee.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 #echo "Enabling APIs..."
 #gcloud services enable cloudbuild.googleapis.com
 
