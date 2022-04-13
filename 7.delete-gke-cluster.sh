5b3699c1ee3c6bf91628b1d000c7864fc890314f
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2022-04-13 14:48:12 -0700

    fix

--- 7.delete-gke-cluster.sh
+++ 7.delete-gke-cluster.sh
@@ -8,18 +8,6 @@ echo "No PROJECT variable set"
 exit
 fi
 
-if [ -z "$NETWORK" ]
-then
-echo "No NETWORK variable set"
-exit
-fi
-
-if [ -z "$SUBNETWORK" ]
-then
-echo "No SUBNETWORK variable set"
-exit
-fi
-
 if [ -z "$LOCATION" ]
 then
 echo "No LOCATION variable set"
@@ -32,19 +20,8 @@ echo "No CLUSTERNAME variable set"
 exit
 fi
 
-if [ -z "$GATEWAY_NAMESPACE" ]
-then
-echo "No GATEWAY_NAMESPACE variable set"
-exit
-fi
 
-echo "Enabling APIs..."
-gcloud services enable compute.googleapis.com
-gcloud services enable container.googleapis.com
-
-echo "Creating cluster..."
+echo "Deleting cluster..."
 gcloud container clusters delete $CLUSTERNAME \
     --project=$PROJECT \
-    --zone=$LOCATION \
-    --network=$NETWORK \
-    --subnetwork=$SUBNETWORK
+    --zone=$LOCATION --quiet
