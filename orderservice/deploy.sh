3df9dca8100a01393a6e594e5995103603291087
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2022-04-18 19:13:56 -0700

    order

--- orderservice/deploy.sh
+++ orderservice/deploy.sh
@@ -10,14 +10,14 @@ kubectl create namespace orders
 ASM_VERSION=$(kubectl get deploy -n istio-system -l app=istiod -o jsonpath={.items[*].metadata.labels.'istio\.io\/rev'}'{"\n"}')
 kubectl label namespace orders istio.io/rev=$ASM_VERSION --overwrite
 
-echo "Deploying orders service"
-kubectl apply -n orders -f ./kubernetes-manifests/orderservice.yaml
-
 echo "Deploying orders service application code"
+kubectl apply -n orders -f ./kubernetes-manifests/orderservice-staging.yaml
 kubectl wait pod -n orders orderservice-staging --for condition=Ready=True --timeout=60s
 kubectl cp -n orders ./dist/Linux/orderservice orderservice-staging:/data/
 kubectl exec -it -n orders orderservice-staging -- bash -c "touch /data/ready"
-
 kubectl delete -n orders pod orderservice-staging
+
+echo "Deploying orders service"
+kubectl apply -n orders -f ./kubernetes-manifests/orderservice.yaml
 
 set +e && popd
